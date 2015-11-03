39d779b242754303595d02d025edd577a5da7e44
Johann <johann.strama@yahoo.de>
2015-11-03 22:09:14 +0100

    nearly ready soo close

--- Exersize1/solution1.py
+++ Exersize1/solution1.py
@@ -11,13 +11,13 @@ def getLabelList(fileName):
 	numberOfLabels = struct.unpack_from(">i",myByteBuffer,4)#">i" stands for big endian 32 bit (4 Byte) Integer required by the file specification  
 	#print(numberOfItems);
 	offsetLabel = 8 # bytes representing the label starts a posstion 8 
-	for i in range(offsetLabel,numberOfLabels[0]):
-		labelList.append(struct.unpack_from("B",myByteBuffer,i)[0])#"B" means unsigned char (one byte).unpack gives always tuple so the first argument can be extracted with [0]	
+	for i in range(0,numberOfLabels[0]):
+		labelList.append(struct.unpack_from("B",myByteBuffer,i+offsetLabel)[0])#"B" means unsigned char (one byte).unpack gives always tuple so the first argument can be extracted with [0]	
 		#print(struct.unpack_from("B",myByteBuffer,i)[0])	
 	return labelList
-	
+
 def getImages(fileName):
-	f = open(fileName,"rb") 	
+	f = open(fileName,"rb")
 	myByteBuffer = bytearray(f.read())
 	numberOfImages = struct.unpack_from(">i",myByteBuffer,4)[0]
 	numberOfColumns = struct.unpack_from(">i",myByteBuffer,8)[0]
@@ -30,73 +30,70 @@ def getImages(fileName):
 	for i in range(0,numberOfImages):#iterate through images 	
 		tempImage = []
 		for j in range(i*bytesPerImage, i*bytesPerImage + bytesPerImage,imageWidth):#Iterate through rows of current Image
-			tempRow = []	
+			tempRow = []
 			for k in range(j,j+imageWidth):#iterate through bytes(greyscale) in rows
 				tempRow.append(struct.unpack_from("B",myByteBuffer,k)[0])
-			tempImage.append(tempRow)	
-		images.append(tempImage)	
+			tempImage.append(tempRow)
+		images.append(tempImage)
 	return images
 #print(getLabelList("data/train-labels.idx1-ubyte"))
-
 #print (getLabelList('data/t10k-labels.idx1-ubyte'))
 def saveImages(byteImageList):
-	i = 0 
+	i = 0
 	for byteImage in byteImageList:
 		i = i+1
 		#print(byteImage)
 		im = Image.new("L",(28,28))
-		im.putdata(byteImage) 
+		im.putdata(byteImage)
 		im.save("images/number" + str(i) +  ".png",'PNG')
 		#print(list(im.getdata()))
 
 
 #byteImageList =  getImages("data/t10k-images.idx3-ubyte")
 def kNearestNeighbours(k,trainingImages,trainingLabels,whatIsIt):
-	print(k)
-	print(whatIsIt[1]) 
-	bytesPerImage = 28 * 28 
-	resultList = []#save euclidean distance and which Label it has in a tupel as a list
-	bucketList = [0,0,0,0,0,0,0,0,0]#A List which saves how many neighbours which specific index there are
-	for i in range(0,len(trainingImage)/28):#prepare 2d-array of bytes to one-diminsial array(26*26 Vector)
-		currentTrainingsVector = []
-		for j in range(0,28): #28 each row from byteImage
-			currentVector.append(trainingImages[j+i*bytesPerImage] #index j define the row of the Image which is located at 28*28(one Image) * i(current processed iamage)	
-			resultList.append((getEuclideanDistance(currentTrainingsVector,whatIsIt[0]),trainingLabels[i])
-	resultList.sort(key=lambda tup: tup[0])#sort by euclidean distance
-	for l in range(0,k):#for every k - neighbours save how much of which label there are 
-		bucketList[resultList[k][1]] +=1	
-	myMax = max(bucketList)#dicision
-	winner = [i for i, j in enumerate(a) if j == m]#making
-	return winner
+    print(k)
+    #print(whatIsIt[1])
+    bytesPerImage = 28 * 28
+    resultList = []#save euclidean distance and which Label it has in a tupel as a list
+    bucketList = [0,0,0,0,0,0,0,0,0]#A List which saves how many neighbours which specific index there are
+    for i in range(0,len(trainingImages)):#prepare 2d-array of bytes to one-diminsial array(26*26 Vector)
+        #print("Which Image: " + str(i) + "lenList: "+ str(len(trainingImages))+ "len labels" + str(len(trainingLabels)))
+        currentTrainingsVector = []
+        myImage=trainingImages[i]
+        for j in range(0,27): #28 each row from byteImage
+            myRow = myImage[j]
+            for e in range(0,27):
+                currentTrainingsVector.append(myRow[e]) #index j define the row of the Image which is located at 28*28(one Image) * i(current processed iamage)	
+        resultList.append((getEuclideanDistance(currentTrainingsVector,whatIsIt[0]),trainingLabels[i]))
+
+    resultList.sort(key=lambda tup: tup[0])#sort by euclidean distance
+    #print(resultList)
+    for l in range(0,k):#for every k - neighbours save how much of which label there are 
+        labelTupel = resultList[l]
+        label = labelTupel[1]
+        bucketList[label] +=1
+    #print(bucketList)
+    myMax = max(bucketList)#dicision
+    winner = [i for i, j in enumerate(bucketList) if j == myMax]#making
+    return winner
 
 def getEuclideanDistance(vector1,vector2):
-	eDistance = 0 
-	for i in range(0,len(vector1)):#for every element of both vector subtract them and add it to ediStance
-		eDistance += math.pow((vector1[i] - vector2[i]),2) 
-	eDistance = math.sqrt(eDistance) #now take the sqaure root
-	return eDistance
+    eDistance = 1
+    for i in range(0,len(vector1)):#for every element of both vector subtract them and add it to ediStance
+        eDistance += math.pow((vector1[i] - vector2[i]),2)
+    eDistance = math.sqrt(eDistance) #now take the sqaure root
+    return eDistance
 
+def aufgabe2():
+    byteImages = getImages("data/t10k-images.idx3-ubyte")
+    labels = getLabelList("data/t10k-labels.idx1-ubyte")
+    kNeighbours = [1,3,11]
+    resultList = [0,0,0]
+    for k in kNeighbours:
+        for n in range(0,100):
+            tupel = ([j for i in byteImages[n] for j in i],labels[n])
+            if(n == kNearestNeighbours(k,byteImages,labels,tupel)):
+                resultList[k] += 1
+    print(resultList)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+aufgabe2()
