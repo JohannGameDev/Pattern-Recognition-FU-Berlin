c7a3606d8f0e5cd3575424b1d20d640322dc319a
Johann <johann.strama@yahoo.de>
2015-11-17 20:44:10 +0100

    cleaned task2a

--- Exersize3/exersize2.py
+++ Exersize3/exersize2.py
@@ -3,8 +3,8 @@ from scipy import special, optimize
 import matplotlib.pyplot as plt
 
 def main():
-   task2a()
-
+   task2aSolution = task2a()
+   task2b(task2aSolution)
 
 def task2a():
     numberOfSamples = 100
@@ -14,13 +14,20 @@ def task2a():
     covarianceMatrix1 = np.matrix(((1,1),(1,1)))
     covarianceMatrix2 = np.matrix(((2,-1),(-1,2)))
     covarianceMatrix3 = np.matrix(((0.1,0),(0,3)))
-    x,y = np.random.multivariate_normal(meanArray1,covarianceMatrix1,numberOfSamples).T
+    generatedPointClass1 = np.random.multivariate_normal(meanArray1,covarianceMatrix1,numberOfSamples)
+    generatedPointClass2 = np.random.multivariate_normal(meanArray2,covarianceMatrix2,numberOfSamples)
+    generatedPointClass3 = np.random.multivariate_normal(meanArray3,covarianceMatrix3,numberOfSamples)
+    x,y = generatedPointClass1.T
     plt.plot(x,y,'x')
-    x,y = np.random.multivariate_normal(meanArray2,covarianceMatrix2,numberOfSamples).T
+    x,y = generatedPointClass2.T
     plt.plot(x,y,'x')
-    x,y = np.random.multivariate_normal(meanArray3,covarianceMatrix3,numberOfSamples).T
+    x,y = generatedPointClass3.T
     plt.plot(x,y,'x')
     plt.axis('equal')
     plt.show()
+    return([generatedPointClass1,generatedPointClass2,generatedPointClass3])
+
+def task2b():
+    print("Here will be task 2b")
 
 main()
