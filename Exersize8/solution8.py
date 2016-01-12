6b1c6826d3ca328079d90f71ce5caa74784d1760
Johann <johann.strama@yahoo.de>
2016-01-12 19:47:12 +0100

    Finished exersize 8

--- Exersize8/solution8.py
+++ Exersize8/solution8.py
@@ -1,10 +1,13 @@
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import random
+import math
 import matplotlib.pyplot as plt
 data = []
 xValues = []
 yValues = []
 beta_list = []
+best_beta = np.array([[0],[0]])
 def main():
     processData()
     old_beta =np.array([[0],[0]])
@@ -14,8 +17,11 @@ def main():
         old_beta = new_beta
         if i in [1,10,100,1000] :
             beta_list.append(new_beta)
-    plot_data()
-def plot_data():
+    best_beta = new_beta
+    plot_data1a()#1a
+    plot_data1c()
+
+def plot_data1a():
     generate_data = []
     list = np.arange(-1.0,2.0,0.01)
     iteration = 1
@@ -33,7 +39,23 @@ def plot_data():
 # fully use the given bounding box.
     plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,ncol=2, mode="expand", borderaxespad=0.)
     plt.show()
-
+def plot_data1c():
+    fakeBestBetaListX = best_beta[0]
+    fakeBestBetaListY = best_beta[1]
+    fakeBestBetaListZ =log_likelihood(fakeBestBetaListX,fakeBestBetaListY) 
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    beta1List = np.arange(-100,100,0.5)
+    beta2List = np.arange(-100,100,0.5)
+    X,Y = np.meshgrid(beta1List,beta2List)
+    zs = np.array([log_likelihood(x,y) for x,y in zip(np.ravel(X), np.ravel(Y))])
+    Z = zs.reshape(X.shape)
+    ax.plot_surface(X, Y, Z)
+    ax.scatter(fakeBestBetaListX,fakeBestBetaListY,fakeBestBetaListZ,c="y",marker="o",s=400)
+    ax.set_xlabel('beta 0 ')
+    ax.set_ylabel('beta 1')
+    ax.set_zlabel('log-likelihood')
+    plt.show()
 def next_gradient(beta):
     local_gradient = np.array([[0],[0]])
     for i in range(0,len(data)):
@@ -43,7 +65,14 @@ def next_gradient(beta):
     return local_gradient
 def probability(x,beta):
     return np.exp(np.dot(np.transpose(beta),x))/(1 +np.exp(np.dot(np.transpose(beta),x)))
-
+def log_likelihood(beta1,beta2):
+    firstSum = 0
+    for i in range(0,len(data)):
+        firstSum += yValues[i] * beta1 + xValues[i] * beta2
+    secondSum = 0
+    for j in range(0,len(data)):
+        secondSum +=math.log1p(1+math.exp(beta1 + beta2*xValues[j]))
+    return firstSum - secondSum
 def processData():
     file = open("data/spiders.txt")
     for line in file:
